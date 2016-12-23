74d0b3997ef37ad47ad41a70e57f2e8d8c033cb3
Alireza5928 <alirezaabedzahe@gmail.com>
2016-12-23 18:47:26 +0330

    Update Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'ernest' then -- Put everything you like :)
+  if matches[1]:lower() == 'alireza' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -143,7 +143,7 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) ([Ee]rnest)",
+    "^[#!/]([Ss]tats) ([alireza)",
 	"^[#!/]([Ee]rnest)"
     }, 
   run = run
